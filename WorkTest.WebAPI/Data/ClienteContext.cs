0cae30bc05c58d36e51bd794020c109828fd769b
Mikhael Pedro <Mikhael Pedro@DESKTOP-FTHISJ2>
2021-03-05 18:11:43 -0300

    $ git remote add origin https://github.com/MikhaelPedro/WorkTest.WebAPI.git

---
+++ WorkTest.WebAPI/Data/ClienteContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkTest.WebAPI.Models;
+
+namespace WorkTest.WebAPI.Data
+{
+    public class ClienteContext : DbContext
+    {
+        //ClienteContext
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; } //= new List<Endereco>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Password=sa123456;Persist Security Info=True;User ID=sa;Initial Catalog=RegisterApp;Data Source=DESKTOP-FTHISJ2");
+        }
+
+    }
+}
